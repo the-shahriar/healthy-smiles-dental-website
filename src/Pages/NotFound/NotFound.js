cd69291230115677190c9248193566328f194381
the-shahriar <theshahriarislam@gmail.com>
2021-10-19 00:01:51 +0600

    404 page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className="flex items-center justify-center my-20">
+            <div className="text-center">
+                <h2 className="text-9xl font-bold tracking-widest">404</h2>
+                <p className="text-lg mt-6">We're sorry, but the page you were looking for doesn't exist</p>
+            </div>
         </div>
     );
 };
