4239c5c6767efd0b2c59b0451c140ce5993adfee
the-shahriar <theshahriarislam@gmail.com>
2021-10-18 23:20:06 +0600

    blog page complete

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FaCalendarAlt } from "react-icons/fa";
+import ImageGallery from '../../components/ImageGallery/ImageGallery';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('./blog.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
-        <div>
-
+        <div className="blog-section">
+            <div className="blogs mx-60 my-10">
+                {
+                    blogs.map(blog => {
+                        const { id, title, image, publish_date, description } = blog;
+                        return (
+                            <div key={id} className="blog my-10">
+                                <img src={image} alt="" />
+                                <div className="mt-6 flex items-center">
+                                    <FaCalendarAlt className="mr-3 text-xl text-purple-500" />
+                                    <p className="text-xl text-purple-500">{publish_date}</p>
+                                </div>
+                                <h2 className="text-4xl font-semibold mb-6 text-green-500">{title}</h2>
+                                <div>
+                                    <p className="text-2xl">{description}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div >
+            {/* Image Gallery Section */}
+            <ImageGallery></ImageGallery>
         </div>
+
     );
 };
 
